878aec9c0342acb05d49d4fa181f782f42d8c386
Joel <joelra@marriottschool.byu.edu>
2014-02-06 09:58:03 -0700

    Added jsoup to begin parsing.

--- TomatoApp/MyApplication/src/main/java/com/garretthendrickson/TomatoApp/Parser.java
+++ TomatoApp/MyApplication/src/main/java/com/garretthendrickson/TomatoApp/Parser.java
@@ -1,4 +1,6 @@
 package com.garretthendrickson.TomatoApp;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 /**
  * Created by somethingPr0fane on 2/6/14.
@@ -6,7 +8,12 @@ package com.garretthendrickson.TomatoApp;
 public class Parser {
     //This will be used to parse HTML into a map for easier manipulation and use.
     public static void main(String html){
+        Document doc;
+        try {
 
+        } catch (Exception ex){
+
+        }
     }
 
 }
