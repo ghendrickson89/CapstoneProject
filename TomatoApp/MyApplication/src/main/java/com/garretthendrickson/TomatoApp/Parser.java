2002d369eeb2d5193bdba6ea1bb8a4e02c7ce4ea
Joel <joelra@marriottschool.byu.edu>
2014-02-06 09:22:32 -0700

    This is a test commit. Added parser class.

---
+++ TomatoApp/MyApplication/src/main/java/com/garretthendrickson/TomatoApp/Parser.java
@@ -0,0 +1,12 @@
+package com.garretthendrickson.TomatoApp;
+
+/**
+ * Created by somethingPr0fane on 2/6/14.
+ */
+public class Parser {
+    //This will be used to parse HTML into a map for easier manipulation and use.
+    public static void main(String html){
+
+    }
+
+}
