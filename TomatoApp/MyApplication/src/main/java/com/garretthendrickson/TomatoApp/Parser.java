12c7ed2f3b61b7b2603758acf0a4adfaf26cd03e
Joel <joelra@marriottschool.byu.edu>
2014-02-06 10:31:35 -0700

    Updated some settings. Running into issues with AppCompat throwing errors on a build.

--- TomatoApp/MyApplication/src/main/java/com/garretthendrickson/TomatoApp/Parser.java
+++ TomatoApp/MyApplication/src/main/java/com/garretthendrickson/TomatoApp/Parser.java
@@ -1,6 +1,7 @@
 package com.garretthendrickson.TomatoApp;
-import org.jsoup.Jsoup;
+
 import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
 
 /**
  * Created by somethingPr0fane on 2/6/14.
@@ -8,8 +9,12 @@ import org.jsoup.nodes.Document;
 public class Parser {
     //This will be used to parse HTML into a map for easier manipulation and use.
     public static void main(String html){
-        Document doc;
+        Document doc = null;
         try {
+            doc = Jsoup.connect("http://www.google.com").get();
+
+            String testTitle = doc.title();
+            System.out.println("Title: " + testTitle);
 
         } catch (Exception ex){
 
